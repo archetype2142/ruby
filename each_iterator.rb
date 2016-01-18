63686825054f8cb6e1ba6bf511ae873149f9189d
Ritwick <malhotraritwick2011@gmail.com>
2016-01-18 21:19:29 +0530

    The .each iterator

--- each_iterator.rb
+++ each_iterator.rb
@@ -9,4 +9,13 @@ end
 # A more useful iterator is the .each method
 # We can apply an expression to each element of an object, one at a time.
 # The syntax looks like this: object.each { |item| # Do something }
+# OR 
+# object.each do |item| 
+# something code 
+# end
 # The variable name between | | can be anything you like
+
+print "\nanother example\n\n"
+odds = [1,3,5,7,9]
+
+odds.each { |x| print "#{x*2}\n" }
