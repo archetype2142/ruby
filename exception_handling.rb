5924a3ecb6ffd98be8bab4330866701b0df29ea7
DumDumGeniuss <dumdumgenius@gmail.com>
2016-01-19 18:01:15 +0800

    Enhance the Exception-Handling , user can input numbers now

--- exception_handling.rb
+++ exception_handling.rb
@@ -1,11 +1,27 @@
-#Simple Exception Handling Example
+#simple Exception Handling Example
+puts 'Enter divident and divisor on this equation'
+puts "result=divident/divisord"
 begin
-  divident=10
-  divisor=0
-  result = divident / divisor
+  num_format=/^[-+]?(\d*[.])?\d+$/
+
+  puts "Enter divident"
+  divident=gets.chomp
+  if (divident =~ num_format).nil?
+    raise "It's not the format of number"
+  end
+
+  puts "Enter divisor"
+  divisor=gets.chomp
+  if (divisor =~ num_format).nil?
+    raise "It's not the format of number"
+  end
+
+  result = divident.to_f / divisor.to_f
   puts result
 rescue ZeroDivisionError
   puts "Warning:Divisor can't be zero"
+rescue Exception => e
+  puts e.message
 end
 
 #This is the easiest way to do try-catch on ruby
